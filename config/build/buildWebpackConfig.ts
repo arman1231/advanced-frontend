a77dab777271f23f0289f6a4538362e20913ed14
Черхаров Арман Андреевич <acherkharov@phoenixit.ru>
2023-04-11 16:08:18 +0300

    add dynamic configuration to webpack

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,24 @@
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import path from 'path'
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoaders'
+import { buildResolvers } from './buildResolvers'
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode} = options;
+    return {
+        mode: mode,
+        entry: paths.entry,
+        module: {
+            rules: buildLoaders(),
+          },
+          resolve: buildResolvers(),
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+      }
+}
