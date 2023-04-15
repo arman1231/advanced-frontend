bc5267154ffaf024e06861b8a41791fceffe39a3
Arman Cherkharov <arman1231@yandex.ru>
2023-04-15 12:01:19 +0300

    add toggle theme & styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import AboutPage from "./pages/AboutPage/AboutPage";
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
-import MainPage from "./pages/MainPage/MainPage";
+import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
