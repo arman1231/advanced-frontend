2c0bfedc82e7e1126313db0db7ba6dc6f23f1b61
Arman Cherkharov <arman1231@yandex.ru>
2023-05-01 21:41:33 +0300

    add plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins({
   paths, isDev
@@ -17,6 +18,7 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new BundleAnalyzerPlugin()
   ];
 }
